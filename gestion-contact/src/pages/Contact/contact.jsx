22953d69d53822616223f2419996d1c1a8dbcbc6
Chaimae aafif <138730457+blackpearlRZ@users.noreply.github.com>
2025-01-10 20:43:11 +0100

    search bar

--- gestion-contact/src/pages/Contact/contact.jsx
+++ gestion-contact/src/pages/Contact/contact.jsx
@@ -5,13 +5,29 @@ export default function Contact (){
     return(
         <>
         <div className="contact">
-            <h1>Contact Page </h1>
-            <input type=" text "  placeholder='Search for your contact'/>
+            <div class="search">
+            <input placeholder="Search" class="search__input" type="text" />
+            <button class="search__button">
+                <svg
+                viewBox="0 0 16 16"
+                class="bi bi-search"
+                fill="currentColor"
+                height="16"
+                width="16"
+                xmlns="http://www.w3.org/2000/svg"
+                >
+                <path
+                    d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"
+                ></path>
+                </svg>
+            </button>
+            </div>
             <div className='list_contact'>
                 {initialState.contacts.map(contact =>(
                     <div className='list_contact_item'>
                     <img src={userPhoto} alt="photo_contact" className='user_photo' />
                     <h4>{contact.fname} {contact.lname}</h4>
+                    <button>More Info</button>
                 </div>
                 ))}
             </div>
