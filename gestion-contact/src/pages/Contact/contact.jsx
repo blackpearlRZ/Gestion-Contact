ea4679c18681446fdd53298d7a3db063a09ef2dc
Nada Zk <nadachan708@gmail.com>
2025-01-12 15:54:42 -0800

    Drt filter l Search bar

--- gestion-contact/src/pages/Contact/contact.jsx
+++ gestion-contact/src/pages/Contact/contact.jsx
@@ -1,54 +1,70 @@
-import './contact.css'
-import { initialState } from '../redux/contacts'
-import userPhoto from '../../assets/user.jpg'
-import { useState } from 'react'
-import ConctactInfo from './conctactInfo'
-import SearchBar from './SearchBar'
+import './contact.css';
+import { initialState } from '../redux/contacts';
+import userPhoto from '../../assets/user.jpg';
+import { useState } from 'react';
+import ConctactInfo from './conctactInfo';
+import SearchBar from './SearchBar';
 
-export default function Contact (){
-    const [clicked , setClicked] = useState('false')
-    const [contact, setContact] = useState(null)
-    function handleInfo(contact){
-        setClicked('true')
-        setContact(contact)
-        console.log(contact)
-    }
-    function handleback(){
-        setClicked('false')
-        setContact(null)
-    }
-    
-    return(
-        <>
-        <div className="contact">
-            <SearchBar/>
-            {clicked == 'false' ? 
-            <div className='list_contact'>
-                {initialState.contacts.map((contact,index) =>(
-                    <div className='list_contact_item' key={index}>
-                    <img src={contact.photo =="" ? userPhoto : contact.photo} alt="photo_contact" className='user_photo' />
-                    <h4>{contact.fname} {contact.lname}</h4>
-                    <button onClick={() => handleInfo(contact)}>More Info</button>
-                </div>
-                ))}
+export default function Contact() {
+  const [clicked, setClicked] = useState(false)
+  const [contact, setContact] = useState(null)
+  const [search, setSearch] = useState('')
+
+  
+  const filteredContacts = initialState.contacts.filter(
+    (contact) =>
+      contact.fname.toLowerCase().includes(search.toLowerCase()) ||
+      contact.lname.toLowerCase().includes(search.toLowerCase())
+  )
+  function handleInfo(contact) {
+    setClicked(true)
+    setContact(contact)
+    console.log(contact)
+  }
+
+  function handleback() {
+    setClicked(false)
+    setContact(null)
+  }
+
+  return (
+    <div className="contact">
+      <SearchBar setSearch={setSearch} />
+      {clicked === false ? (
+        <div className="list_contact">
+          {filteredContacts.map((contact, index) => (
+            <div className="list_contact_item" key={index}>
+              <img
+                src={contact.photo === '' ? userPhoto : contact.photo}
+                alt="photo_contact"
+                className="user_photo"
+              />
+              <h4>
+                {contact.fname} {contact.lname}
+              </h4>
+              <button onClick={() => handleInfo(contact)}>More Info</button>
             </div>
-            : <div className='contact_info'>
-            <h1>More Info</h1>
-            <div>
+          ))}
+        </div>
+      ) : (
+        <div className="contact_info">
+          <h1>More Info</h1>
+          <div>
             {contact && (
-                    <ul >
-                        <li>First Name: {contact.fname}</li>
-                        <li>Last Name: {contact.lname}</li>
-                        <li>Phone: {contact.phone}</li>
-                    </ul>
-                )}
+              <ul>
+                <li>First Name: {contact.fname}</li>
+                <li>Last Name: {contact.lname}</li>
+                <li>Phone: {contact.phone}</li>
+              </ul>
+            )}
             <button>Modifier</button>
             <button>Supprimer</button>
-            </div>
-            <button className='back_btn' onClick={() => handleback()}>Back to Contact</button>
-            </div> 
-            }
+          </div>
+          <button className="back_btn" onClick={handleback}>
+            Back to Contact
+          </button>
         </div>
-        </>
-    )
+      )}
+    </div>
+  )
 }
