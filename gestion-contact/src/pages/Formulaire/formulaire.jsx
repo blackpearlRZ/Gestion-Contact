77595d8aabd84cf21c5e193b96d7016361649a2c
Chaimae aafif <138730457+blackpearlRZ@users.noreply.github.com>
2025-01-12 14:46:19 +0100

    formulaire

--- gestion-contact/src/pages/Formulaire/formulaire.jsx
+++ gestion-contact/src/pages/Formulaire/formulaire.jsx
@@ -1,29 +1,30 @@
 import "./formulaire.css";
 export default function Formulaire() {
+
   return (
     <>
       <div className="formulaire">
-        <form action="">
+        <form>
           <h1>Fill Up This Form</h1>
-          <div>
-            <label>Prénom:</label>
-            <input type="text" placeholder="Prénom" value={firstName} />
-          </div>
-          <div>
-            <label>Nom de famille:</label>
-            <input type="text" placeholder="Nom de famille" value={lastName} />
-          </div>
-          <div>
-            <label>Téléphone:</label>
-            <input type="text" placeholder="Téléphone" value={phone} />
-          </div>
-          <div>
-            <label>Email:</label>
-            <input type="email" placeholder="Email" value={email} />
-          </div>
           <div>
             <label>Photo:</label>
             <input type="file" accept="image/*" />
+          </div>
+            <label>Prénom:</label>
+            <input type="text" placeholder="Prénom"  />
+          <div>
+          </div>
+          <div>
+            <label>Nom de famille:</label>
+            <input type="text" placeholder="Nom de famille"  />
+          </div>
+          <div>
+            <label>Téléphone:</label>
+            <input type="text" placeholder="Téléphone"  />
+          </div>
+          <div>
+            <label>Email:</label>
+            <input type="email" placeholder="Email"  />
           </div>
           <button type="submit">Enregistrer</button>
         </form>
