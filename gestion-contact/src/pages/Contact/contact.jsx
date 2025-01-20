5a6f6442be598477d72b254d3b454a4c7703fecf
saad <saaddaimi404@gmail.com>
2025-01-20 22:01:36 +0100

    supprimer and search

--- gestion-contact/src/pages/Contact/contact.jsx
+++ gestion-contact/src/pages/Contact/contact.jsx
@@ -1,79 +1,107 @@
-import './contact.css'
-import userPhoto from '../../assets/User.png'
-import { useState } from 'react'
-import { useNavigate} from 'react-router-dom'
-import { useSelector , useDispatch} from "react-redux"
+import "./contact.css";
+import userPhoto from "../../assets/User.png";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
-export default function Contact (){
-    const Navigate = useNavigate()
-    const [clicked , setClicked] = useState('false')
-    const [contact, setContact] = useState(null)
-    const state = useSelector(state => state.contacts)
-    const dispatch = useDispatch()
-    function handleInfo(contact){
-        setClicked('true')
-        setContact(contact)
-        console.log(contact)
-    }
-    function handleback(){
-        setClicked('false')
-        setContact(null)
-    }
-    function backForm(id){
-        dispatch({
-            type : 'SET_ID',
-            payload : id
-        })
-        Navigate("/Formulaire")
-    }
-    
-    return(
-        <>
-        <div className="contact">
-            <div className="search">
-                <input placeholder="Search" className="search__input"/>
-                <button className="search__button">
-                    <svg
-                    viewBox="0 0 16 16"
-                    className="bi bi-search"
-                    fill="currentColor"
-                    height="16"
-                    width="16"
-                    xmlns="http://www.w3.org/2000/svg"
-                    >
-                    <path
-                        d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"
-                    ></path>
-                    </svg>
-                </button>
-            </div>
-            {clicked == 'false' ? 
-            <div className='list_contact'>
-                {state.map((contact,index) =>(
-                    <div className='list_contact_item' key={index}>
-                        <img src={contact.photo =="" ? userPhoto : contact.photo} alt="photo_contact" className='user_photo' />
-                        <h4>{contact.fname} {contact.lname}</h4>
-                        <button onClick={() => handleInfo(contact)}>More Info</button>
-                    </div> 
-                ))}
-            </div>
-            : <div className='contact_info'>
+export default function Contact() {
+  const Navigate = useNavigate();
+  const [clicked, setClicked] = useState("false");
+  const [contact, setContact] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const state = useSelector((state) => state.contacts);
+  const dispatch = useDispatch();
+  function handleInfo(contact) {
+    setClicked("true");
+    setContact(contact);
+    console.log(contact);
+  }
+  function handleback() {
+    setClicked("false");
+    setContact(null);
+  }
+  function handleDelete(id) {
+    dispatch({
+      type: "DELETE_CONTACT",
+      payload: id,
+    });
+    setClicked("false");
+    setContact(null);
+  }
+  function backForm(id) {
+    dispatch({
+      type: "SET_ID",
+      payload: id,
+    });
+    Navigate("/Formulaire");
+  }
+  //aearch
+  const filteredContacts = state.filter((contact) => {
+    const fullName = `${contact.fname} ${contact.lname}`.toLowerCase();
+    return fullName.includes(searchTerm.toLowerCase());
+  });
+
+  return (
+    <>
+      <div className="contact">
+        <div className="search">
+          <input
+            placeholder="Search"
+            className="search__input"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button className="search__button">
+            <svg
+              viewBox="0 0 16 16"
+              className="bi bi-search"
+              fill="currentColor"
+              height="16"
+              width="16"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"></path>
+            </svg>
+          </button>
+        </div>
+        {clicked == "false" ? (
+          <div className="list_contact">
+            {filteredContacts.map((contact, index) => (
+              <div className="list_contact_item" key={index}>
+                <img
+                  src={contact.photo == "" ? userPhoto : contact.photo}
+                  alt="photo_contact"
+                  className="user_photo"
+                />
+                <h4>
+                  {contact.fname} {contact.lname}
+                </h4>
+                <button onClick={() => handleInfo(contact)}>More Info</button>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="contact_info">
             <h1>More Info</h1>
             <div>
-            {contact && (
-                    <ul >
-                        <li>First Name: {contact.fname}</li>
-                        <li>Last Name: {contact.lname}</li>
-                        <li>Phone: {contact.phone}</li>
-                    </ul>
-            )}
-            <button onClick={() =>backForm(contact.id)}>Modifier</button>
-            <button>Supprimer</button>
+              {contact && (
+                <ul>
+                  <li>First Name: {contact.fname}</li>
+                  <li>Last Name: {contact.lname}</li>
+                  <li>Phone: {contact.phone}</li>
+                </ul>
+              )}
+              <button onClick={() => backForm(contact.id)}>Modifier</button>
+              <button onClick={() => handleDelete(contact.id)}>
+                Supprimer
+              </button>
             </div>
-            <button className='back_btn' onClick={() => handleback()}>Back to Contact</button>
-            </div> 
-            }
-        </div>
-        </>
-    )
+            <button className="back_btn" onClick={() => handleback()}>
+              Back to Contact
+            </button>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
--- gestion-contact/src/pages/redux/contactReducers.js
+++ gestion-contact/src/pages/redux/contactReducers.js
@@ -1,42 +1,43 @@
 import { initialState } from "./contacts";
 
-export function ContactReducers(state = initialState, action){
-    switch(action.type){
-        case 'ADD_CONTACT':
+export function ContactReducers(state = initialState, action) {
+  switch (action.type) {
+    case "ADD_CONTACT":
+      return {
+        ...state,
+        contacts: [...state.contacts, action.payload],
+      };
+    case "DELETE_CONTACT":
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          (contact) => contact.id !== action.payload
+        ),
+      };
+    case "UPDATE_CONTACT":
+      return {
+        ...state,
+        contacts: state.contacts.map((contact) => {
+          if (contact.id === action.payload.id) {
             return {
-                ...state,
-                contacts : [...state.contacts,action.payload]
-            }
-        case 'DELETE_CONTACT':
-            return {
-                ...state,
-                contacts : state.contacts.filter(contact => contact.id !== action.payload.id)
-            }
-        case 'UPDATE_CONTACT':
-            return{
-                ...state,
-                contacts : state.contacts.map(contact => {
-                    if(contact.id == action.payload.id){
-                        return{
-                            ...contact,
-                            fname : action.payload.fname,
-                            lname : action.payload.lname,
-                            email : action.payload.email,
-                            phone : action.payload.phone,
-                            photo : action.payload.photo
-                        }
-                    }
-                    else {
-                        return contact
-                    }
-                })
-            }
-        case 'SET_ID':
-            return {
-                ...state,
-                id : action.payload
-            }
-        default :
-            return state
-    }
+              ...contact,
+              fname: action.payload.fname,
+              lname: action.payload.lname,
+              email: action.payload.email,
+              phone: action.payload.phone,
+              photo: action.payload.photo,
+            };
+          } else {
+            return contact;
+          }
+        }),
+      };
+    case "SET_ID":
+      return {
+        ...state,
+        id: action.payload,
+      };
+    default:
+      return state;
+  }
 }
